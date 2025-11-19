fb2a9f02c80b9568e9838b97fc6e9dd2a397ee1d
JinHyeok <jinhyeok@unist.ac.kr>
2025-11-19 14:34:49 +0000

    add vggt script

--- script/nvos_single.sh
+++ script/nvos_single.sh
@@ -3,12 +3,12 @@ i=0
 wandb_group=tmp
 data_path=/root/data1/jinhyeok/ludvig/dataset/llff_data
 
-gpus=(2)
+gpus=(3)
 for data in fern; do
     gpu_id=${gpus[$((i % ${#gpus[@]}))]}
     echo "gpu:$gpu_id data:$data"
     
-    CUDA_VISIBLE_DEVICES=$gpu_id bash script/seg.sh $data $wandb_group dif_NVOS 2>&1 | tee terminal/$data.log &
+    CUDA_VISIBLE_DEVICES=$gpu_id bash script/seg_vggt.sh $data $wandb_group dif_NVOS 2>&1 | tee terminal/$data.log &
     # CUDA_VISIBLE_DEVICES=$gpu_id bash script/seg.sh $data $wandb_group dif_NVOS
 
     i=$((i + 1))
---
+++ script/seg_vggt.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+scene=$1
+wandb_group=$2
+
+src_path="SPIn-NeRF_data"
+[[ "$scene" == @(fern|orchids|trex|horns|flower|fortress|leaves|room|horns_center|horns_left) ]] && src_path="llff_data"
+height=1199
+width=1600
+scene_tag=${scene}
+if [ "$scene" = "fork" ]; then
+    height=1202
+elif [ "$scene" = "room" ]; then
+    height=1200
+elif [ "$scene" = "truck" ]; then
+    width=979; height=546
+elif [ "$scene" = "lego" ]; then
+    width=1015; height=764
+elif [ "$scene" = "horns_center" ]; then
+    scene="horns"
+elif [ "$scene" = "horns_left" ]; then
+    scene="horns"
+elif [ "$scene" = "trex" ]; then
+    width=1599
+fi
+
+python ludvig_uplift.py \
+    --colmap_dir ./dataset/${src_path}/$scene/vggt/ \
+    --gs_source ./dataset/${src_path}/$scene/vggt/point_cloud/iteration_0/point_cloud.ply \
+    --config configs/$3.yaml \
+    --height $height \
+    --width $width \
+    --tag ${scene_tag} \
+    --wandb \
+    --wandb_group ${wandb_group} \
+
+
+    
